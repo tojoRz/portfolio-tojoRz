0ca6c96e0820be4b5467b988bc0baec36156d995
tojoRz <toojrazakanirina01@gmail.com>
2024-05-03 14:59:47 +0300

    update header

--- src/app/header/nav/nav.component.ts
+++ src/app/header/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faList } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nav',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class NavComponent {
 
+  faList = faList
 }
